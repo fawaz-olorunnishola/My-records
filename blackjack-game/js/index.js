92deb9405cbaecc71d10c21c03c02b359681e48b
Fawas Olorunnishola <fawasolorunnishola9@gmail.com>
2023-11-20 09:14:02 +0100

    Update 17-11-2023

--- blackjack-game/js/index.js
+++ blackjack-game/js/index.js
@@ -1,21 +1,38 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 3
 let hasBlackJack = false
-let sum = firstCard + secondCard + 4
+let sum = firstCard + secondCard 
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cadrEl  = document.getElementById("card-el")
 
-if (sum < 21) {
-    message = ("Do you to draw new card")
-} else if (sum === 21) {
-    message = ("Wohoo! You have blackjack")
-    hasBlackJack = true
-}else if (sum > 21) {
-    message = ("You 're out of the game")
-    isAlive = false
+function startGame () {
+    renderGame()
 }
 
-console.log(message)
+function renderGame() {
+    cadrEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum 
+    if (sum <= 20) {
+        message = "Do you to draw new card"
+    } else if (sum === 21) {
+        message = "You 've got blackjack"
+        hasBlackJack = true
+    } else {
+        message = "You 're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newcard() {
+    let card = 6
+    sum += card
+    renderGame()
+}
+
 
 
 
