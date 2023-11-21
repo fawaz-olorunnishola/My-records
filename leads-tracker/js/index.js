5d9a6356f1ddf76671336a8f1cba8af2ab57d6b5
Fawas Olorunnishola <fawasolorunnishola9@gmail.com>
2023-11-21 23:02:56 +0100

    Update 21-11-2023

--- leads-tracker/js/index.js
+++ leads-tracker/js/index.js
@@ -1,2 +1,29 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItem = " "
+    for (let i = 0; i < myLeads.length; i++) {
+        // listItem += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItem += `<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>`
+
+    }
+    ulEl.innerHTML = listItem
+}
+
+
+
+
+// Creating an Element, set textcontent, append to ul
+// const li = document.createElement("li")
+// li.textContent = myLeads[i]
+// ulEl.append(li)
