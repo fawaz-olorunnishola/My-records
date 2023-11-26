c952c32d8f86daa3323119182f202f15eb0e7feb
Fawas Olorunnishola <fawasolorunnishola9@gmail.com>
2023-11-26 23:25:39 +0100

    Updated On this days

--- leads-tracker/js/index.js
+++ leads-tracker/js/index.js
@@ -2,26 +2,63 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+let leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads",))
+let tabBtn = document.getElementById("tab-btn")
+
+if (leadFromLocalStorage) {
+    myLeads = leadFromLocalStorage
+    render(myLeads)
+}
 
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+tabBtn.addEventListener("click", function() { 
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
 })
 
-function renderLeads() {
-    let listItem = " "
-    for (let i = 0; i < myLeads.length; i++) {
-        // listItem += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-        listItem += `<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>`
+function render(leads) {
+    let listItem = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItem +=
+            `
+    <li>
+            <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
+            </a>
+        </li>
+        `
 
     }
     ulEl.innerHTML = listItem
 }
 
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
 
 
+
+
+
+
+
+// myLeads = JSON.stringify(myLeads)
+
+// console.log(typeof myLeads)
 
 // Creating an Element, set textcontent, append to ul
 // const li = document.createElement("li")
